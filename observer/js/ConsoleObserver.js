9adc212aa04fddb442c9c3ef8fb3ba70a524310c
Vikash S <vikash.shaw@imerit.net>
2019-02-07 19:31:55 +0530

    observer pattern js

---
+++ observer/js/ConsoleObserver.js
@@ -0,0 +1,11 @@
+class ConsoleObserver{
+
+    constructor(){
+
+    }
+
+    update(model){
+        console.log('The number is ' + model.number + 
+        ' and the color is ' + model.color.toUpperCase());
+    }
+}
---
+++ observer/js/NumberModel.js
@@ -0,0 +1,27 @@
+class NumberModel {
+    
+    constructor(){
+        this.number = 0;
+        this.color = 'red';
+        this.observers = [];
+    }
+
+    increment(){
+        const colors = ['red', 'orange', 'green', 'blue'];
+        this.number++;
+
+        this.color = colors[Math.floor(Math.random()*colors.length)];
+        
+        this.notifyObservers();
+    }
+
+    addObserver(o){
+        this.observers.push(o);
+    }
+
+    notifyObservers(){
+        for(let o of this.observers){
+            o.update(this);
+        }
+    }
+}
